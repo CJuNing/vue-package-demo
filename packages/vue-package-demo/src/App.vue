cb015b7537c6790f617c44c08529592753e2ddfc
Shawn0079 <zhaoyongsheng@gwssi.com.cn>
2021-01-04 14:55:21 +0800

    11

---
+++ packages/vue-package-demo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="map">
+    <Main :propData="initData" />
+  </div>
+</template>
+
+<script>
+import Main from "./components/Main";
+export default {
+  data() {
+    return {
+      initData: "hello 你好",
+    };
+  },
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style>
+.map {
+  margin: 100px;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+</style>
---
+++ packages/vue-package-demo/vue.config.js
@@ -0,0 +1,17 @@
+var path = require('path')
+
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+    // 修改打包入口
+    entry: NODE_ENV == 'development' ? './src/main.js' : './packages/switch/index.js',
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '../dist/',
+        filename: 'custom-switch.js',
+        library: 'custom-switch', // 指定的就是你使用require时的模块名
+        libraryTarget: 'umd', // libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的
+        umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
+    }
+}
