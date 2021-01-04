062e4f6b16d23da9d243b901aee4cfc1253f341f
Shawn0079 <zhaoyongsheng@gwssi.com.cn>
2021-01-04 20:05:27 +0800

    11111111

--- hooks/check-keyword.sh
+++ hooks/check-keyword.sh
@@ -10,6 +10,7 @@ yellow=`tput setaf 3`
 echo  "${green}pre-commit: 开始检查非法关键字${reset}"
 
 result=0
+badjs=0
 
 for FILE in `git diff --name-only --cached`; do
 
@@ -21,12 +22,19 @@ for FILE in `git diff --name-only --cached`; do
     if [[ $FILE != *".js"* &&  $FILE != *".vue"* ]];then
         continue
     fi
-    a=`grep -c 'debugger\|alert(' $FILE`
+    a=`grep -c 'debugger\|^alert(' $FILE`
     if [ $? -eq 0 ]; then
+        badjs=`expr ${badjs} + 1`
         echo "${red}pre-commit: 错误!文件${yellow}" $FILE "${red}中包含${yellow} ${a} ${red}处非法字符,请删除后重试!${reset}"
         # grep -n 'debugger\|alert(' $FILE 
         result=1
     fi
     
 done
+
+if [[ $result == 0 ]];then
+    echo "${cyan}pre-commit: 检测通过未发现非法字符${reset}"
+else
+    echo "${red}pre-commit: 共检测到${yellow} ${badjs} ${red}个文件包含非法字符${reset}"
+fi
 exit ${result}
