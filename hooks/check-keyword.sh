7022e26105852e130bddebbd4cba19aaa8290503
Shawn0079 <zhaoyongsheng@gwssi.com.cn>
2021-01-04 14:56:39 +0800

    11

---
+++ hooks/check-keyword.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+# echo "${red}red text ${green}green text${reset}"
+
+echo  "${green}start checking keyword${reset}"
+
+
+for FILE in `git diff --name-only --cached`; do
+
+    if [[ $FILE == *".sh"* ]] || [[ $FILE == *"vendor/*"* ]] ||[[$FILE == *"node_modules/*"*]]||[[$FILE == *"public/*"*]]; then
+        # echo $FILE
+        continue
+    fi
+    # grep 'TODO:\|debugger\|console.log\|alert(' $FILE 2>&1 >/dev/null
+    grep 'debugger' $FILE 2>&1 >/dev/null
+    if [ $? -eq 0 ]; then
+        echo $FILE '提交代码中包含"debugger",请删除后重试!'
+        exit 1
+    fi
+    
+done
+exit
