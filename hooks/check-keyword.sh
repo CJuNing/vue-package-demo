185f61406abeb17478ea64f6f51313326b165d34
Shawn0079 <zhaoyongsheng@gwssi.com.cn>
2021-01-04 16:39:57 +0800

    11111111

--- hooks/check-keyword.sh
+++ hooks/check-keyword.sh
@@ -5,7 +5,7 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 # echo "${red}red text ${green}green text${reset}"
 
-echo  "${green}precommit: 开始检查非法关键字${reset}"
+echo  "${green}pre-commit: 开始检查非法关键字${reset}"
 
 result=0
 
@@ -16,9 +16,22 @@ for FILE in `git diff --name-only --cached`; do
         continue
     fi
     # grep 'TODO:\|debugger\|console.log\|alert(' $FILE 2>&1 >/dev/null
-    grep 'debugger' $FILE 2>&1 >/dev/null
+    # grep 'debugger' $FILE 2>&1 >/dev/null
+    # if [ $? -eq 0 ]; then
+    #     echo "${red}"$FILE "中包含 'debugger',请删除后重试!${reset}"
+    #     result=1
+    # fi
+    # grep 'alert(' $FILE 2>&1 >/dev/null
+    # if [ $? -eq 0 ]; then
+    #     echo "${red}文件" $FILE "中包含 'alert' 方法,请删除后重试!${reset}"
+    #     result=1
+    # fi
+    # grep -n 'debugger\|alert(' $FILE rrravs
+    grep 'debugger\|alert(' $FILE 2>&1 >/dev/null
+    grep -n 'debugger\|alert('
     if [ $? -eq 0 ]; then
-        echo "${red}" $FILE "中包含 'debugger',请删除后重试!${reset}"
+    echo $?
+        echo "${red}pre-commit: 文件" $FILE "中包含 非法关键字 ,请删除后重试!${reset}"
         result=1
     fi
     
